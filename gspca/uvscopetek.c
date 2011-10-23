e1d423c46e7d09d11236f659f5e45ef37d9ca1ad
John McMaster <JohnDMcMaster@gmail.com>
2011-10-22 23:07:14 -0700

    testable state but gspca gives "no transfer endpoint found"

--- gspca/uvscopetek.c
+++ gspca/uvscopetek.c
@@ -1,6 +1,10 @@
 /*
- * Benq DC E300 subdriver
+ * AnchorChips / ScopeTek / AmScope  DCM and MDC camera driver
  *
+ * Copyright (C) 2011 John McMaster <JohnDMcMaster@gmail.com>
+ *
+ * Original copyright:
+ * Benq DC E300 subdriver
  * Copyright (C) 2009 Jean-Francois Moine (http://moinejf.free.fr)
  *
  * This program is free software; you can redistribute it and/or modify
@@ -26,6 +30,14 @@ MODULE_AUTHOR("John McMaster");
 MODULE_DESCRIPTION("Scopetek DCM/MDC high resolution camera driver");
 MODULE_LICENSE("GPL");
 
+#define sdbg( _format, ... ) printk( KERN_INFO "uvscopetek: " _format "\n", ## __VA_ARGS__ )
+//#define sdbg_replay sdbg
+#define sdbg_replay(...)
+
+static unsigned int g_bytes = 0;
+static unsigned int g_frame_size = 640 * 480 * 3;
+
+
 /* specific webcam descriptor */
 struct sd {
 	struct gspca_dev gspca_dev;	/* !! must be the first item */
@@ -35,37 +47,19 @@ struct sd {
 static const struct ctrl sd_ctrls[] = {
 };
 
+//Andrew's guess
+#define PIX_FMT		V4L2_PIX_FMT_SGBRG8
+//Website
+//#define PIX_FMT		4L2_PIX_FMT_RGB24
 static const struct v4l2_pix_format vga_mode[] = {
-	{320, 240, V4L2_PIX_FMT_JPEG, V4L2_FIELD_NONE,
+	{640, 480, PIX_FMT, V4L2_FIELD_NONE,
 		.bytesperline = 320,
-		.sizeimage = 320 * 240 * 3 / 8 + 590,
-		.colorspace = V4L2_COLORSPACE_JPEG},
+		.sizeimage = 640 * 480 * 3 / 8,
+		.colorspace = V4L2_COLORSPACE_SRGB},
 };
 
 static void sd_isoc_irq(struct urb *urb);
 
-/* -- write a register -- */
-static void reg_w(struct gspca_dev *gspca_dev,
-			u16 value, u16 index)
-{
-	struct usb_device *dev = gspca_dev->dev;
-	int ret;
-
-	if (gspca_dev->usb_err < 0)
-		return;
-	ret = usb_control_msg(dev, usb_sndctrlpipe(dev, 0),
-			0x02,
-			USB_DIR_OUT | USB_TYPE_VENDOR | USB_RECIP_DEVICE,
-			value,
-			index,
-			NULL,
-			0,
-			500);
-	if (ret < 0) {
-		PDEBUG(D_ERR, "reg_w err %d", ret);
-		gspca_dev->usb_err = ret;
-	}
-}
 
 /* this function is called at probe time */
 static int sd_config(struct gspca_dev *gspca_dev,
@@ -84,17 +78,36 @@ static int sd_init(struct gspca_dev *gspca_dev)
 	return 0;
 }
 
-static int sd_isoc_init(struct gspca_dev *gspca_dev)
-{
-	int ret;
-
-	ret = usb_set_interface(gspca_dev->dev, gspca_dev->iface,
-		gspca_dev->nbalt - 1);
-	if (ret < 0) {
-		err("usb_set_interface failed");
-		return ret;
+int validate_read(void *expected, size_t expected_size, void *actual, size_t actual_size, const char *msg) {
+	if (expected_size != actual_size) {
+		printk(KERN_ALERT "%s: expected %d bytes, got %d bytes\n", msg, expected_size, actual_size);
+		return -1;
 	}
-/*	reg_w(gspca_dev, 0x0003, 0x0002); */
+	if (memcmp(expected, actual, expected_size)) {
+		printk(KERN_ALERT "%s: regions do not match\n", msg);
+		return -1;
+	}
+	sdbg_replay("%s: validate ok", msg);
+	return 0;
+}
+
+int validate_write( unsigned int size, int n_rw, const char *desc) {
+	if (size != n_rw) {
+		sdbg("write %s failed: expected %u got %d",
+				desc, size, n_rw);
+		return -1;
+	}
+	return 0;
+}
+
+int replay_wireshark_setup_neo(struct gspca_dev *gspca_dev) {
+	struct usb_device *udev = gspca_dev->dev;
+	
+	sdbg("neo replay");
+	{
+#include "replay.c"
+	}
+	sdbg("neo replay done");
 	return 0;
 }
 
@@ -104,13 +117,16 @@ static int sd_start(struct gspca_dev *gspca_dev)
 	struct urb *urb;
 	int i, n;
 
-	/* create 4 URBs - 2 on endpoint 0x83 and 2 on 0x082 */
+	g_bytes = 0;
+	replay_wireshark_setup_neo(gspca_dev);
+
+	/* create 2 URBs on endpoint 0x082 */
 #if MAX_NURBS < 4
 #error "Not enough URBs in the gspca table"
 #endif
 #define SD_PKT_SZ 64
 #define SD_NPKT 32
-	for (n = 0; n < 4; n++) {
+	for (n = 0; n < 2; n++) {
 		urb = usb_alloc_urb(SD_NPKT, GFP_KERNEL);
 		if (!urb) {
 			err("usb_alloc_urb failed");
@@ -130,7 +146,7 @@ static int sd_start(struct gspca_dev *gspca_dev)
 		urb->context = gspca_dev;
 		urb->transfer_buffer_length = SD_PKT_SZ * SD_NPKT;
 		urb->pipe = usb_rcvisocpipe(gspca_dev->dev,
-					n & 1 ? 0x82 : 0x83);
+					0x82);
 		urb->transfer_flags = URB_ISO_ASAP
 					| URB_NO_TRANSFER_DMA_MAP;
 		urb->interval = 1;
@@ -145,16 +161,6 @@ static int sd_start(struct gspca_dev *gspca_dev)
 	return gspca_dev->usb_err;
 }
 
-static void sd_stopN(struct gspca_dev *gspca_dev)
-{
-	reg_w(gspca_dev, 0x003c, 0x0003);
-	reg_w(gspca_dev, 0x003c, 0x0004);
-	reg_w(gspca_dev, 0x003c, 0x0005);
-	reg_w(gspca_dev, 0x003c, 0x0006);
-	reg_w(gspca_dev, 0x003c, 0x0007);
-	usb_set_interface(gspca_dev->dev, gspca_dev->iface, gspca_dev->nbalt - 1);
-}
-
 static void sd_pkt_scan(struct gspca_dev *gspca_dev,
 			u8 *data,		/* isoc packet */
 			int len)		/* iso packet length */
@@ -166,7 +172,6 @@ static void sd_pkt_scan(struct gspca_dev *gspca_dev,
 static void sd_isoc_irq(struct urb *urb)
 {
 	struct gspca_dev *gspca_dev = (struct gspca_dev *) urb->context;
-	struct urb *urb0;
 	u8 *data;
 	int i, st;
 
@@ -183,28 +188,24 @@ static void sd_isoc_irq(struct urb *urb)
 		PDEBUG(D_ERR|D_PACK, "urb status: %d", urb->status);
 		return;
 	}
-
-	/* if this is a control URN (ep 0x83), wait */
-	if (urb == gspca_dev->urb[0] || urb == gspca_dev->urb[2])
-		return;
-
-	/* scan both received URBs */
-	if (urb == gspca_dev->urb[1])
-		urb0 = gspca_dev->urb[0];
-	else
-		urb0 = gspca_dev->urb[2];
+	
 	for (i = 0; i < urb->number_of_packets; i++) {
-
+		unsigned int data_len = 0;
 		/* check the packet status and length */
-		if (urb0->iso_frame_desc[i].actual_length != SD_PKT_SZ
-		    || urb->iso_frame_desc[i].actual_length != SD_PKT_SZ) {
+		/*
+		my lengths aren't picky, I take what I can get
+		they seem to float everywhere
+		
+		if (urb->iso_frame_desc[i].actual_length != SD_PKT_SZ) {
 			PDEBUG(D_ERR, "ISOC bad lengths %d / %d",
-				urb0->iso_frame_desc[i].actual_length,
 				urb->iso_frame_desc[i].actual_length);
 			gspca_dev->last_packet_type = DISCARD_PACKET;
 			continue;
 		}
-		st = urb0->iso_frame_desc[i].status;
+		*/
+		
+		data_len = urb->iso_frame_desc[i].actual_length;
+		st = urb->iso_frame_desc[i].status;
 		if (st == 0)
 			st = urb->iso_frame_desc[i].status;
 		if (st) {
@@ -214,49 +215,37 @@ static void sd_isoc_irq(struct urb *urb)
 			gspca_dev->last_packet_type = DISCARD_PACKET;
 			continue;
 		}
-
-		/*
-		 * The images are received in URBs of different endpoints
-		 * (0x83 and 0x82).
-		 * Image pieces in URBs of ep 0x83 are continuated in URBs of
-		 * ep 0x82 of the same index.
-		 * The packets in the URBs of endpoint 0x83 start with:
-		 *	- 80 ba/bb 00 00 = start of image followed by 'ff d8'
-		 *	- 04 ba/bb oo oo = image piece
-		 *		where 'oo oo' is the image offset
-						(not cheked)
-		 *	- (other -> bad frame)
-		 * The images are JPEG encoded with full header and
-		 * normal ff escape.
-		 * The end of image ('ff d9') may occur in any URB.
-		 * (not cheked)
-		 */
-		data = (u8 *) urb0->transfer_buffer
-					+ urb0->iso_frame_desc[i].offset;
-		if (data[0] == 0x80 && (data[1] & 0xfe) == 0xba) {
-
-			/* new image */
-			gspca_frame_add(gspca_dev, LAST_PACKET,
-					NULL, 0);
-			gspca_frame_add(gspca_dev, FIRST_PACKET,
-					data + 4, SD_PKT_SZ - 4);
-		} else if (data[0] == 0x04 && (data[1] & 0xfe) == 0xba) {
-			gspca_frame_add(gspca_dev, INTER_PACKET,
-					data + 4, SD_PKT_SZ - 4);
-		} else {
-			gspca_dev->last_packet_type = DISCARD_PACKET;
-			continue;
-		}
 		data = (u8 *) urb->transfer_buffer
 					+ urb->iso_frame_desc[i].offset;
-			gspca_frame_add(gspca_dev, INTER_PACKET,
-					data, SD_PKT_SZ);
+		
+		//if a frame is in progress see if we can finish it off
+		//do {
+			if (g_bytes < g_frame_size) {
+				if (g_frame_size + data_len >= g_frame_size) {
+					unsigned int remainder = g_frame_size - data_len;
+					//Completed a frame
+					gspca_frame_add(gspca_dev, LAST_PACKET,
+							data, remainder);
+					g_frame_size = 0;
+					data_len -= remainder;
+					data += remainder;
+					g_bytes = 0;
+				}
+			}
+			if (g_bytes > 0) {
+				if (g_bytes == 0) {
+					gspca_frame_add(gspca_dev, FIRST_PACKET,
+							data, data_len);
+				} else {
+					gspca_frame_add(gspca_dev, INTER_PACKET,
+							data, data_len);
+				}
+				g_bytes += data_len;
+			}
+		//} while (data_len > 0);
 	}
 
 	/* resubmit the URBs */
-	st = usb_submit_urb(urb0, GFP_ATOMIC);
-	if (st < 0)
-		PDEBUG(D_ERR|D_PACK, "usb_submit_urb(0) ret %d", st);
 	st = usb_submit_urb(urb, GFP_ATOMIC);
 	if (st < 0)
 		PDEBUG(D_ERR|D_PACK, "usb_submit_urb() ret %d", st);
@@ -269,15 +258,16 @@ static const struct sd_desc sd_desc = {
 	.nctrls = ARRAY_SIZE(sd_ctrls),
 	.config = sd_config,
 	.init = sd_init,
-	.isoc_init = sd_isoc_init,
 	.start = sd_start,
-	.stopN = sd_stopN,
 	.pkt_scan = sd_pkt_scan,
 };
 
+#define UVSCOPETEK_VENDOR_ID	0x0547
+#define UVSCOPETEK_PRODUCT_ID	0x4D88
+
 /* -- module initialisation -- */
 static const __devinitdata struct usb_device_id device_table[] = {
-	{USB_DEVICE(0x04a5, 0x3035)},
+	{USB_DEVICE(0x0547, 0x4D88)},
 	{}
 };
 MODULE_DEVICE_TABLE(usb, device_table);
@@ -320,3 +310,4 @@ static void __exit sd_mod_exit(void)
 
 module_init(sd_mod_init);
 module_exit(sd_mod_exit);
+
