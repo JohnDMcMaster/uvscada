4816924836da9b711e11c350ed251a3e8f27400e
John McMaster <JohnDMcMaster@gmail.com>
2011-10-28 00:13:46 -0700

    plotted pixel proper decoder

--- decode/decode.py
+++ decode/decode.py
@@ -75,39 +75,132 @@ def decode_SGBRG8():
 	
 	
 	f = open(image_in, "r")
-	image = Image.new("RGB", (width, height), "White")
 	
 	# Skip offset
 	#f.read(254)
-	f.read(1)
-	f.read(640 * 480 * 3)
-	
-	for y in range(0, height, 2):
-		# GBGB
-		# RGRG
-		line0 = f.read(width)
-		#line0 = f.read(width)
-		#line0 = f.read(width)
+	f.read(640 / 2 + 300)
+	f.read(640 * 480 * 2)
 		
-		line1 = f.read(width)
-		#line1 = f.read(width)
-		#line1 = f.read(width)
-		for x in range(0, width):
-			R = ord(line1[x])
-			G = ord(line0[x])
-			# make even
-			B = ord(line0[x - (x % 2)])
-			image.putpixel((x, y), (R, G, B))
-		for x in range(0, width):
-			R = ord(line1[x])
-			# Make odd
-			G = ord(line1[x - (x % 2) + 1])
-			# make even
-			B = ord(line0[x - (x % 2)])
-			image.putpixel((x, y + 1), (R, G, B))
-	image.show()
+	image = None
+	while True:
+		image = Image.new("RGB", (width, height), "White")
+		for y in range(0, height, 2):
+			# GBGB
+			# RGRG
+			line0 = f.read(width)
+			#line0 = f.read(width)
+			#line0 = f.read(width)
+		
+			line1 = f.read(width)
+			#line1 = f.read(width)
+			#line1 = f.read(width)
+		
+			if y < 0:
+				print '%03u start: 0x%02X, 0x%02X, 0x%02X, 0x%02X' % (y, ord(line0[0]), ord(line0[1]), ord(line0[2]), ord(line0[3]))
+				print '%03u start: 0x%02X, 0x%02X, 0x%02X, 0x%02X' % (y + 1, ord(line1[0]), ord(line1[1]), ord(line1[2]), ord(line1[3]))
+			if y < 0:
+				b = 640 - 4
+				print '%03u   end: 0x%02X, 0x%02X, 0x%02X, 0x%02X' % (y, ord(line0[b + 0]), ord(line0[b + 1]), ord(line0[b + 2]), ord(line0[b + 3]))
+				print '%03u   end: 0x%02X, 0x%02X, 0x%02X, 0x%02X' % (y + 1, ord(line1[b + 0]), ord(line1[b + 1]), ord(line1[b + 2]), ord(line1[b + 3]))
+
+		
+			for x in range(0, width):
+				R = ord(line1[x])
+				G = ord(line0[x])
+				# make even
+				B = ord(line0[x - (x % 2)])
+				if image:
+					image.putpixel((x, y), (R, G, B))
+			for x in range(0, width):
+				R = ord(line1[x])
+				# Make odd
+				G = ord(line1[x - (x % 2) + 1])
+				# make even
+				B = ord(line0[x - (x % 2)])
+				if image:
+					image.putpixel((x, y + 1), (R, G, B))
+		if image:
+			image.show()
+			break
+		print
+		print
+		print
+
+
+def decode_as_sensor():
+	'''
+	Require interpolation
+	Read two lines at a time
+	'''
+	width = 640
+	#width = 642
+	height = 480
+	
+	
+	f = open(image_in, "r")
+	
+	# Skip offset
+	#f.read(254)
+	#f.read(640 / 2 + 300)
+	f.read(640 * 480 * 2)
+		
+	image = None
+	while True:
+		image = Image.new("RGB", (width, height), "White")
+		for y in range(0, height, 2):
+			# GBGB
+			# RGRG
+			line0 = f.read(width)
+			#line0 = f.read(width)
+			#line0 = f.read(width)
+		
+			line1 = f.read(width)
+			#line1 = f.read(width)
+			#line1 = f.read(width)
+		
+			if y < 0:
+				print '%03u start: 0x%02X, 0x%02X, 0x%02X, 0x%02X' % (y, ord(line0[0]), ord(line0[1]), ord(line0[2]), ord(line0[3]))
+				print '%03u start: 0x%02X, 0x%02X, 0x%02X, 0x%02X' % (y + 1, ord(line1[0]), ord(line1[1]), ord(line1[2]), ord(line1[3]))
+			if y < 0:
+				b = 640 - 4
+				print '%03u   end: 0x%02X, 0x%02X, 0x%02X, 0x%02X' % (y, ord(line0[b + 0]), ord(line0[b + 1]), ord(line0[b + 2]), ord(line0[b + 3]))
+				print '%03u   end: 0x%02X, 0x%02X, 0x%02X, 0x%02X' % (y + 1, ord(line1[b + 0]), ord(line1[b + 1]), ord(line1[b + 2]), ord(line1[b + 3]))
+
+			if True:
+				temp = line0
+				line0 = line1
+				line1 = temp
+		
+			for x in range(0, width):
+				R = 0
+				if x % 2 == 0:
+					G = 0
+					B = ord(line0[x])
+				else:
+					G = ord(line0[x])
+					B = 0
+					
+				if image:
+					image.putpixel((x, y), (R, G, B))
+			for x in range(0, width):
+				B = 0
+				if x % 2 == 0:
+					R = 0
+					G = ord(line1[x])
+				else:
+					R = ord(line1[x])
+					G = 0
+				
+				if image:
+					image.putpixel((x, y + 1), (R, G, B))
+		if image:
+			image.show()
+			break
+		print
+		print
+		print
 
 #decode_orig()
 #decode2()
-decode_SGBRG8()
-
+#decode_SGBRG8()
+decode_as_sensor()
