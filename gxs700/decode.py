5275b2af6d2bbf38af67c8e95182a5bd4aed626b
John McMaster <JohnDMcMaster@gmail.com>
2015-10-03 18:13:31 -0700

    gxs decode: dir based option

--- gxs700/decode.py
+++ gxs700/decode.py
@@ -2,6 +2,8 @@
 import argparse
 import numpy as np
 import struct
+import os
+import glob
 
 from uvscada import gxs700
 
@@ -40,6 +42,17 @@ def histeq(buff, nbr_bins=256):
         ret += struct.pack('>H', int(rs[i]))
     return str(ret)
 
+def process(fin, fout):
+    print 'Reading %s...' % fin
+    buff = open(fin, 'r').read()
+    if args.hist_eq:
+        print 'Equalizing histogram...'
+        buff = histeq(buff)
+    print 'Decoding image...'
+    img = gxs700.GXS700.decode(buff)
+    print 'Saving %s...' % fout
+    img.save(fout)
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Replay captured USB packets')
     parser.add_argument('--hist-eq', '-e', action='store_true', help='Equalize histogram')
@@ -47,20 +60,18 @@ if __name__ == "__main__":
     parser.add_argument('fout', default=None, nargs='?', help='File name out')
     args = parser.parse_args()
 
-    if args.fout is None:
-        if args.fin.find('.bin') < 0:
-            raise Exception("Can't guess output file name")
-        args.fout = args.fin.replace('.bin', '.png')
-        if args.hist_eq:
-            args.fout = args.fout.replace('.png', '_e.png')
-
-    print 'Reading %s...' % args.fin
-    buff = open(args.fin, 'r').read()
-    if args.hist_eq:
-        print 'Equalizing histogram...'
-        buff = histeq(buff)
-    print 'Decoding image...'
-    img = gxs700.GXS700.decode(buff)
-    print 'Saving image...'
-    img.save(args.fout)
+    if os.path.isdir(args.fin):
+        if args.fout is None:
+            raise Exception("dir requires fout")
+        for fn in glob.glob(os.path.join(args.fin, '*.bin')):
+            fout = os.path.join(args.fout, os.path.basename(fn).replace('.bin', '.png'))
+            process(fn, fout)
+    else:
+        if args.fout is None:
+            if args.fin.find('.bin') < 0:
+                raise Exception("Can't guess output file name")
+            args.fout = args.fin.replace('.bin', '.png')
+            if args.hist_eq:
+                args.fout = args.fout.replace('.png', '_e.png')
+        process(args.fin, args.fout)
     print 'Done'
