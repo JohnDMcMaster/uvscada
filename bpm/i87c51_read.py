0c7ec2f44f8d7b7a7bf5bbb6f63dd1c7f2d0613d
John McMaster <JohnDMcMaster@gmail.com>
2015-10-20 19:43:20 -0700

    bpm read: byte frequency analysis

--- bpm/i87c51_read.py
+++ bpm/i87c51_read.py
@@ -8,6 +8,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Replay captured USB packets')
     add_bool_arg(parser, '--cycle', default=False, help='') 
     add_bool_arg(parser, '--cont', default=True, help='Continuity check') 
+    add_bool_arg(parser, '--freq', default=False, help='Byte frequency distribution') 
     parser.add_argument('fout', nargs='?', help='Output file') 
     args = parser.parse_args()
 
@@ -23,5 +24,13 @@ if __name__ == "__main__":
     else:
         hexdump(fw_in, indent='  ', label='Read data')
     print 'Bytes: %d 0x%04X' % (len(fw_in), len(fw_in))
+    
+    if args.freq:
+        freqs = {}
+        for b in fw_in:
+            freqs[b] = freqs.get(b, 0) + 1
+        print 'Freq'
+        for k, v in freqs.iteritems():
+            print '  0x%02X: 0x%04X' % (ord(k), v)
 
     print 'Complete'
