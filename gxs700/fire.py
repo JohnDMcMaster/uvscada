6bf444f5972f20ae23d5312068800c60d3dc7dfb
John McMaster <JohnDMcMaster@gmail.com>
2015-11-20 21:01:45 -0800

    x-ray fire: use standard WPS7 class, add command line args

--- gxs700/fire.py
+++ gxs700/fire.py
@@ -1,29 +1,29 @@
 import pycurl
 import time
 import os
+from uvscada.wps7 import WPS7
+import argparse
 
 SW_HV = 1
 SW_FIL = 2
 
-def switch(n, on):
-    state = 'ON' if on else 'OFF'
-    c = pycurl.Curl()
-    c.setopt(c.URL, 'http://energon/outlet?%d=%s' % (n, state))
-    c.setopt(c.WRITEDATA, open('/dev/null', 'w'))
-    c.setopt(pycurl.USERPWD, '%s:%s' % (os.getenv('WPS7_USER', 'admin'), os.getenv('WPS7_PASS', '')))
-    c.perform()
-    c.close()
+if __name__ == '__main__':    
+    parser = argparse.ArgumentParser(description='Planner module command line')
+    parser.add_argument('--fil', type=int, default=5, help='Filament warm time')
+    parser.add_argument('--hv', type=int, default=3, help='HV fire time')
+    args = parser.parse_args()
 
-print 'Warming filament...'
-switch(SW_FIL, 1)
-time.sleep(5)
-
-print 'Nuking...'
-switch(SW_HV, 1)
-time.sleep(3)
-
-print 'Safing...'
-switch(SW_HV, 0)
-
-print 'Done'
-
+    wps = WPS7(host='energon')
+    
+    try:
+        print 'Warming filament %d sec...' % args.fil
+        wps.on(SW_FIL)
+        time.sleep(args.fil)
+        
+        print 'Nuking %d sec...' % args.hv
+        wps.on(SW_HV)
+        time.sleep(args.hv)
+    finally:
+        print 'Forcing HV off at exit'
+        wps.off(SW_HV)
+    print 'Done'
