1121a8bfe4981c234595da0b973b0838c7c24ae3
John McMaster <JohnDMcMaster@gmail.com>
2015-12-08 08:39:13 -0800

    pwrmap

--- ezlaze/pwrmap.py
+++ ezlaze/pwrmap.py
@@ -13,7 +13,7 @@ import csv
 import datetime
 import os
 
-def main(hal, dmm, el):
+def main1(hal, dmm, el):
     # 50x max ap size
     # Very approx
     #SPOT = 0.06
@@ -76,13 +76,81 @@ def main(hal, dmm, el):
     tend = time.time()
     print 'Took %s' % time_str(tend - tstart)
 
+def main2(hal, dmm, el):
+    # 50x max ap size
+    # Very approx
+    #SPOT = 0.06
+
+    shut_open = el.shut_open
+    if 0:
+        # 10x
+        SPOT = 0.3
+        el.shut_square(args.square)
+    if 1:
+        # 10x reduced
+        SPOT = 0.05
+        # From trial and error
+        def shut_open():
+            el.shut_square(30)
+    
+    hal.mv_abs({'x': -SPOT, 'y': -SPOT})
+    
+    if not args.dry:
+        cwf = open(args.fout, 'wb')
+    
+    cols = int(4.6 / SPOT)
+    rows = int(3.8 / SPOT)
+    tpic = 2.3
+    npic = cols * rows
+    print 'Taking %dc x %dr => %d pics => ETA %s' % (cols, rows, npic, time_str(tpic * npic))
+    tstart = time.time()
+    
+    # Takes some time to settle
+    # Close early
+    el.shut_close()
+    for row in xrange(rows):
+        hal.mv_abs({'x': -SPOT, 'y': row * SPOT})
+        for col in xrange(cols):
+            hal.mv_abs({'x': col * SPOT})
+            if args.dry:
+                continue
+            print '%s %dc, %dr: ' % (datetime.datetime.utcnow(), col, row),
+
+            closed = []
+            for i in xrange(1):
+                t = time.time()
+                closed.append((t, dmm.curr_dc()))
+            
+            shut_open()
+            opened = []
+            for i in xrange(4):
+                time.sleep(0.5)
+                t = time.time()
+                curr = dmm.curr_dc()
+                opened.append((t, curr))
+                delta = curr - closed[0][1] 
+                print '  %0.9f A' % delta,
+                
+            cwf.write(repr({'col': col, 'row': row, 'open': opened, 'close': closed}) + '\n')
+            print
+            el.shut_close()
+            cwf.flush()
+
+    print 'Ret home'
+    hal.mv_abs({'x': 0, 'y': 0})
+    print 'Movement done'
+    # Convenient to be open at end to retarget
+    shut_open()
+    tend = time.time()
+    print 'Took %s' % time_str(tend - tstart)
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Use ezlaze with LinuxCNC to carve a bitmap')
-    parser.add_argument('--dmm', default='/dev/ttyUSB0', help='K2750 serial port')
-    parser.add_argument('--laser', default='/dev/ttyUSB1', help='ezlaze serial port')
+    parser.add_argument('--dmm', default='/dev/serial/by-id/usb-Prologix_Prologix_GPIB-USB_Controller_PX8ZBY4W-if00-port0', help='K2750 serial port')
+    parser.add_argument('--laser', default='/dev/serial/by-id/usb-1a86_USB2.0-Ser_-if00-port0', help='ezlaze serial port')
     parser.add_argument('--host', default='mk-bs', help='LinuxCNC host')
     parser.add_argument('--dry', action='store_true', help='Dry run')
-    parser.add_argument('fout', nargs='?', default='pwrmap.csv', help='Store data to')
+    parser.add_argument('fout', nargs='?', default='pwrmap.pyv', help='Store data to')
     args = parser.parse_args()
 
     if os.path.exists(args.fout):
@@ -105,7 +173,7 @@ if __name__ == "__main__":
 
         print
         print 'Running'
-        main(hal, dmm, el)
+        main2(hal, dmm, el)
     finally:
         print 'Shutting down hal'
         if hal:
