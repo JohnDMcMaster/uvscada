3d0c8d95992f85c559d44bca6cbea1cc05ba5429
John McMaster <johndmcmaster@gmail.com>
2016-10-23 17:52:46 -0700

    nuc/plt enhancements

--- nuc/plt.py
+++ nuc/plt.py
@@ -1,19 +1,53 @@
+import argparse
 import matplotlib.pyplot as plt
 import json
 
-f = open('2016-10-20_01_cs-137.csv', 'r')
-data = []
-print 'Loading'
-for l in f:
-    try:
-        j = json.loads(l)
-    except:
-        break
-    data.append(j['v'])
+def load_csv(f):
+    f = open(f, 'r')
+    data = []
+    print 'Loading'
+    for l in f:
+        try:
+            j = json.loads(l)
+        except:
+            break
+        data.append(j['v'])
+    return data
 
-print 'Plotting (%d samples)' % (len(data),)
-plt.hist(data, bins=range(0, 32768, 32))
-plt.show()
+def load_jl(f):
+    f = open(f, 'r')
+    data = []
+    print 'Loading'
+    for l in f:
+        try:
+            j = json.loads(l)
+        except:
+            break
+        data.append(j['v'])
+    return data
 
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Help')
+    parser.add_argument('fns', nargs='+', help='Files')
+    args = parser.parse_args()
+
+    print 'Looping'
+
+    for fn in args.fns:
+        print
+        print fn
+        fn_out = fn.replace('.jl', '.png')
+        if fn_out == fn:
+            raise Exception()
+        data = load_jl(fn)
+
+        limit = 32768
+        limit = 25000
+        data = filter(lambda x: x < limit, data)
+
+        print 'Plotting (%d samples)' % (len(data),)
+        plt.hist(data, bins=range(0, limit, 32))
+        plt.savefig(fn_out)
+        #plt.show()
 
 
